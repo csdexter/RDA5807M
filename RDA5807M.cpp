c8a96752aec9d10171d827cc45e099a3c35b1b9b
Radu - Eosif Mihailescu <radu.mihailescu@linux360.ro>
2015-04-01 23:11:00 +0100

    Initial commit  - parsed datasheet and various sources of information in the wild into the header file  - code to follow

---
+++ RDA5807M.cpp
@@ -0,0 +1,15 @@
+/* Arduino RDA5807M Library
+ * See the README file for author and licensing information. In case it's
+ * missing from your distribution, use the one here as the authoritative
+ * version: https://github.com/csdexter/RDA5807M/blob/master/README
+ *
+ * This library is for interfacing with a RDA Microelectronics RDA5807M
+ * single-chip FM broadcast radio receiver.
+ * See the example sketches to learn how to use the library in your code.
+ *
+ * This is the main code file for the library.
+ * See the header file for better function documentation.
+ */
+
+#include "RDA5807M.h"
+
--- README.md
+++
@@ -1,2 +0,0 @@
-# RDA5807M
-RDA Microelectronics RDA5807M FM Stereo/RDS single-chip receiver HAL library for Arduino 
